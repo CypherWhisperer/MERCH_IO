66d5f61df598f49ca9f9b69da6a733ce11aed748
CypherWhisperer <cypherwhisperer@gmail.com>
2025-11-18 00:55:03 +0300

    FIX: fixed the routes for Vercel hosting

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,22 +1,32 @@
 // This file is a single source of truth and management for the Project routes
-// This makes it easier to manage and later add routes in the project 
+// This makes it easier to manage and later add routes in the project
 
 // importing the Pages Components
-import { Landing, SignUpIn, Home, Product, Cart, Cashout, ProductTrack, Admin, NotFound } from './pages/pages.js'
+import {
+  Admin,
+  Cart,
+  Cashout,
+  Home,
+  Landing,
+  NotFound,
+  Product,
+  ProductTrack,
+  SignUpIn,
+} from "./pages/pages.js";
 
 // Decalring and exporting the routes array
 export const routes = [
-  { path: '/', element: <Landing /> },
-  { path: 'signUpIn', element: <SignUpIn /> },
-  { path: 'home', element: <Home /> },
-  { path: 'product', element: <Product /> },
-  { path: 'cart', element: <Cart /> },
-  { path: 'cashout', element: <Cashout /> },
-  { path: 'productTrack', element: <ProductTrack /> },
-  { path: 'admin', element: <Admin /> },
-  { path: '*', element: <NotFound /> },
-]
+  { path: "/", element: <Landing /> },
+  { path: "/signUpIn", element: <SignUpIn /> },
+  { path: "/home", element: <Home /> },
+  { path: "/product", element: <Product /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/cashout", element: <Cashout /> },
+  { path: "/productTrack", element: <ProductTrack /> },
+  { path: "/admin", element: <Admin /> },
+  { path: "*", element: <NotFound /> },
+];
 
-/* Dynamic Routing For Dynamic Products 
+/* Dynamic Routing For Dynamic Products
   <Route path="product/:productId" element={<Product props={props} />} />
 */
